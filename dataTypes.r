0636ee1964c1bf7c25976f7add41c63f8c3e924d
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-02-11 10:21:34 +0530

    Add files via upload

---
+++ dataTypes.r
@@ -0,0 +1,16 @@
+a = 4
+b = 'c'
+c = "Hello"
+d = as.integer(5)
+e = a>d
+f = TRUE
+g = 4 + 3i
+
+
+print(class(a))
+print(class(b))
+print(class(c))
+print(class(d))
+print(class(e))
+print(class(f))
+print(class(g))
