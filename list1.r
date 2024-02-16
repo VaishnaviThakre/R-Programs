4b442d7789766b572b02059ee802cf55c0b982c8
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-02-16 08:21:05 +0530

    Add files via upload

---
+++ list1.r
@@ -0,0 +1,13 @@
+
+empId = c(1, 2, 3, 4)
+empName = c("Debi", "Sandeep", "Subham", "Shiba")
+numberOfEmp = 4
+empList = list(
+  "ID" = empId,
+  "Names" = empName,
+  "Total Staff" = numberOfEmp
+)
+print(empList)
+
+cat("Accessing name components using $ command\n")
+print(empList$Names)
